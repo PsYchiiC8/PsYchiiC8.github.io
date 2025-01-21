eb04fca1ea438f5572ffafaf4578a2cf98520529
PsYchiiC8 <kevinhuang2887@gmail.com>
2025-01-20 23:22:59 -0500

    Reinitialize repository with new root

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body className="body">
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+
+          <h1 className="home">Welcome to My Developer Page</h1>
+
+          <br></br>
+          <br></br>
+
+          <p className="home">"Learn as if you will live forever,<br></br> live like you will die tomorrow"<br></br>-Mahatma Gandhi</p>
+
+          <br></br>
+          <br></br>
+
+          <p className="home">
+            Hello there!<br></br>
+            
+            Thank you for visiting my website.<br></br>
+            Please feel free to go through<br></br>
+            my pages and learn about me!
+          </p>
+        </body>
+    </div>
+  );
+}
+
+export default App;
+
+  
